e2feda8450eb75a840246495e1aee2bb2ec86b53
Zach Schaffer <cursedhydra@gmail.com>
2018-05-25 03:36:51 -0400

    Add project files.

---
+++ ItemGen/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing.Text;
+using System.Security.Cryptography.X509Certificates;
+
+public class Item
+{
+    enum Type : int 
+    {
+        Weapon
+    }
+    private String name;
+    private Type type;
+
+	public Item()
+	{
+        this.type = Type.Weapon;
+	}
+}
